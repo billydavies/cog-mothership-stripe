21385c737a1c13dfe3cf43d4a7bf84c6a769b57a
thomasjthomasj <thomasjmarchant@hotmail.com>
2014-10-17 16:51:14 +0100

    do not allow customers who log out or switch user to make payments

---
+++ src/Exception/PayableNotValidException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Message\Mothership\Stripe\Exception;
+
+/**
+ * Exception for use when validating Payable instances. Error messages are customer-facing.
+ *
+ * Class PayableNotValidException
+ * @package Message\Mothership\Stripe\Exception
+ */
+class PayableNotValidException extends \LogicException
+{}
